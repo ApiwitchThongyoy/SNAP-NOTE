2f69e39576577d22d5b1c2a230ef9fdcce267533
ApiwitchThongyoy <67026506@up.ac.th>
2025-09-03 22:36:28 +0700

    ใส่CSS ในหน้า Reset password

--- src/component/Resetpassword/ResetDetail.jsx
+++ src/component/Resetpassword/ResetDetail.jsx
@@ -4,7 +4,7 @@ import { BsArrowLeftCircleFill } from "react-icons/bs";
 
 function ResetPassword() {
   const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState("");              
+  const [otp, setOtp] = useState("");
   const [generatedOtp, setGeneratedOtp] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -34,8 +34,7 @@ function ResetPassword() {
     }
 
     alert(`รีเซ็ตรหัสผ่านสำเร็จ!\nEmail: ${email}`);
-    
-    // ล้างค่า input
+
     setEmail("");
     setOtp("");
     setNewPassword("");
@@ -44,38 +43,53 @@ function ResetPassword() {
   }
 
   return (
-    <div class = "flex justify-center items-center w-screen min-h-screen  bg-[#56A750]">
+    <div className="relative flex min-h-screen bg-[#56A750]">
       <div>
-        <button>
+        <button className="absolute top-4 right-20 text-6xl text-[#164C11] cursor-pointer z-50">
           <BsArrowLeftCircleFill />
         </button>
       </div>
 
-      <img src={Logo_Login} alt="logo" />
-      <div>
-        <h1>Reset Password</h1>
-        <form onSubmit={handleSubmit}>
+      <div className="absolute left-0 top-0 h-full">
+        <img
+          src={Logo_Login}
+          alt="left-side"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      <div className="ml-auto w-1/2 p-8 flex flex-col justify-center z-10">
+        <h1 className="text-3xl font-bold mb-6 text-start text-xxl text-[#164C11]">
+          Reset Password
+        </h1>
+
+        <form onSubmit={handleSubmit} className="flex flex-col">
           <div>
-            Email<br />
+            Email
+            <br />
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your E-mail"
               required
+              className="w-[550px] p-3 border rounded mb-4 text-sm bg-[#BDFFA7]"
             />
           </div>
 
           <div>
             <button
               type="button"
-              onClick={handleRequestOtp} 
+              onClick={handleRequestOtp}
+              className="w-[550px] bg-[#164C11] text-white py-3 rounded hover:bg-green-600 transition-colors cursor-pointer font-bold mb-4"
             >
+              รับรหัส OTP
             </button>
           </div>
 
           <div>
-            OTP<br />
+            OTP
+            <br />
             <input
               type="text"
               value={otp}
@@ -83,32 +97,42 @@ function ResetPassword() {
               placeholder="Enter 6-digit OTP"
               maxLength={6}
               required
+              className="w-[550px] p-3 border rounded mb-4 text-sm bg-[#BDFFA7]"
             />
           </div>
 
           <div>
-            New Password<br />
+            New Password
+            <br />
             <input
               type="password"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
               placeholder="Create New Password"
               required
+              className="w-[550px] p-3 border rounded mb-4 text-sm bg-[#BDFFA7]"
             />
           </div>
 
           <div>
-            Confirm Password<br />
+            Confirm Password
+            <br />
             <input
               type="password"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
               placeholder="Confirm your Password"
               required
+              className="w-[550px] p-3 border rounded mb-4 text-sm bg-[#BDFFA7]"
             />
           </div>
 
-          <button type="submit">Reset</button>
+          <button
+            type="submit"
+            className="w-[550px] bg-[#164C11] text-white py-3 rounded hover:bg-green-600 transition-colors cursor-pointer font-bold"
+          >
+            Reset
+          </button>
         </form>
       </div>
     </div>
