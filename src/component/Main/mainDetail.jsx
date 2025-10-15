af5df8f7a1bb7e0ae2c3aa1324c79b13b40c4cdd
Ratibodi <ratibodee02@gmail.com>
2025-10-15 16:06:59 +0700

    โฆษณา

--- src/component/Main/mainDetail.jsx
+++ src/component/Main/mainDetail.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { PostContext } from "../../context/PostContext";
 import {
   BsBell,
@@ -156,8 +156,47 @@ export default function MainDetail() {
 
         {/* Ads */}
         <div className="w-1/5 bg-[#434343] p-6 flex items-center justify-center rounded-xl sticky top-4 max-h-[calc(95.7vh-6rem)]">
-          <h2>โฆษณา</h2>
+          <AdCarousel/>
         </div>
+      </div>
+    </div>
+  );
+}
+function AdCarousel() {
+  const ads = [
+   "src/assets/0tbi8w.jpg",
+   "src/assets/1756624415_699135.png",
+   "src/assets/document_7198_p8_20211012142423.jpg"
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % ads.length);
+    }, 5000); // เปลี่ยนภาพทุก 3 วิ
+    return () => clearInterval(interval);
+  }, [ads.length]);
+
+  return (
+    <div className="relative w-full h-120 flex items-center justify-center">
+      {/* รูปโฆษณา */}
+      <img
+        src={ads[currentIndex]}
+        alt="ad"
+        className="w-full h-full object-cover rounded-xl transition-all duration-700 ease-in-out"
+      />
+
+      {/* จุดแสดงสถานะภาพ */}
+      <div className="absolute bottom-2 flex gap-2">
+        {ads.map((_, i) => (
+          <div
+            key={i}
+            className={`w-3 h-3 rounded-full transition-all ${
+              i === currentIndex ? "bg-green-400" : "bg-gray-500"
+            }`}
+          ></div>
+        ))}
       </div>
     </div>
   );
