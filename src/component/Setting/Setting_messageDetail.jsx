70b24e82687b5a4bc3ab7e9aa130058dcab4f80a
Max <maxma4056@gmail.com>
2025-10-19 12:58:53 +0700

    Fix setrting agian
    delete private and account

--- src/component/Setting/Setting_messageDetail.jsx
+++ src/component/Setting/Setting_messageDetail.jsx
@@ -74,28 +74,12 @@ function Setting_messageDetail() {
                 ทั่วไป
               </button>
               <button className="w-full rounded-3xl hover:bg-gray-200 active:bg-gray-300 p-2 text-sm cursor-pointer"
-              onClick={() => navigate("/settingaaccount")}
-              >
-                สลับบัญชี
-              </button>
-              <button className="w-full rounded-3xl hover:bg-gray-200 active:bg-gray-300 p-2 text-sm cursor-pointer"
               onClick={() => navigate("")}
               >
                 การแจ้งเตือน
               </button>
-              <button className="w-full rounded-3xl hover:bg-gray-200 active:bg-gray-300 p-2 text-sm cursor-pointer"
-              onClick={() => navigate("/settingprivate")}
-              >
-                ความเป็นส่วนตัว
-              </button>
-            </div>
-            <h2 className="font-semibold mt-4 mb-2">ความเป็นส่วนตัวและนโยบาย</h2>
-            <div className="flex flex-col gap-2">
-              <button className="w-full rounded-3xl hover:bg-gray-200 active:bg-gray-300 p-2 text-sm cursor-pointer">
-                การแจ้งเตือน
-              </button>
-              <button className="w-full rounded-3xl hover:bg-gray-200 active:bg-gray-300 p-2 text-sm cursor-pointer">
-                ความเป็นส่วนตัว
+              <button className="w-full rounded-3xl bg-red-500 hover:bg-red-600 active:bg-red-600 p-2 text-sm cursor-pointer mt-8">
+                ออกจากระบบ
               </button>
             </div>
           </div>
